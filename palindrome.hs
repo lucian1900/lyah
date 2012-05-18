23b3866fc2ba1b4bc9333a3595841327118a5c15
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2012-05-18 13:02:02 +0100

    Palindrome checker

---
+++ palindrome.hs
@@ -0,0 +1,15 @@
+#!/usr/bin/runhaskell
+
+isPalindrome :: String -> Bool
+isPalindrome input = input == reverse input
+
+palindromeDecision :: String -> String
+palindromeDecision input =
+	if isPalindrome input
+		then "palindrome"
+		else "not a palindrome"
+
+palindromeLines :: String -> String
+palindromeLines = unlines . (map palindromeDecision) . lines
+
+main = interact $ palindromeLines
