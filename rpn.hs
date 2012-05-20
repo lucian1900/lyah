ca1688085878819c3baafb01f25c6701364fc67a
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-05-20 02:32:27 +0100

    More functions in rpn

--- rpn.hs
+++ rpn.hs
@@ -1,8 +1,12 @@
 #!/usr/bin/runhaskell
 
-solveRPN :: (Num a, Read a) => String -> a
+solveRPN :: String -> Float
 solveRPN = head . foldl compute [] . words
     where compute (x:y:ys) "*" = (x * y):ys
           compute (x:y:ys) "+" = (x + y):ys
           compute (x:y:ys) "-" = (y - x):ys
+          compute (x:y:ys) "/" = (y / x):ys
+          compute (x:y:ys) "^" = (y ** x):ys
+          compute (x:xs) "ln" = log x:xs
+          compute xs "sum" = [sum xs]
           compute xs numberString = read numberString:xs
